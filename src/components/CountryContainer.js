5cea979f5722d3efdc0899363798b0e9291e4387
“KelvinShamy” <kelvinshamy@gmail.com>
2022-12-22 11:56:38 -0500

    1st commit after github creation

--- src/components/CountryContainer.js
+++ src/components/CountryContainer.js
@@ -14,6 +14,7 @@ class CountryContainer extends Component {
         this.changeCurrCountry = this.changeCurrCountry.bind(this);
     }
     
+    // BLAH BLAH BLAH BLAH BLAH
    
     changeCurrCountry(newCountry) {
         this.setState({ currCountry: newCountry })
