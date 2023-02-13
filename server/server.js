76e20f4f8f91cd4e53a5cc62cc5c4f685c5e24ff
“KelvinShamy” <kelvinshamy@gmail.com>
2023-02-13 00:27:44 -0500

    refactor server

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const models = require('./models/mongoModels');
 
 const MONGO_URI = "mongodb+srv://kiosk:1234@cluster0.fadk0p7.mongodb.net/solo?retryWrites=true&w=majority"
-const PORT = 3000; 
+const PORT = 3500; 
 
 // const apiRouter = require();  IS THIS STEP NECESSARY?
 
@@ -17,7 +17,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', res.status(200).sendFile(path.join(__dirname, '../src/index.html')));
+// app.use('/', express.static('src'));
+app.get('/', (req, res) => res.status(200).sendFile(path.join(__dirname, '../src/index.html')));
 
 // app.get('/test', (req, res) => {
 //     res.send('Hello from our Express server');
