cbece73e61ddf0924020e1bf46a3886e3179b2bb
“KelvinShamy” <kelvinshamy@gmail.com>
2023-03-11 19:45:59 -0500

    refactor CountrySelector to hooks

--- src/components/CountrySelector.js
+++ src/components/CountrySelector.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import defaultCountries from 'i18n-iso-countries/langs/en.json';
 import { HiCheckCircle } from 'react-icons/hi';
 // const dotenv = require('dotenv').config();
@@ -7,21 +7,15 @@ import { HiCheckCircle } from 'react-icons/hi';
 // const PORT = process.env.PORT;
 const PORT = 3505;
 
-class CountrySelector extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            favs: true,
-            favsClicked: false,
-            favsList: []
-        }
-        this.getFavs = this.getFavs.bind(this);
-        this.deletor = this.deletor.bind(this);
-        this.visitUnvisit = this.visitUnvisit.bind(this);
-    }
+const CountrySelector = (props) => {
+    // HOOKS NEEDED
+    const [favsList, setFavsList] = useState([]);
+    // these two may or may not be needed/optimal
+    const [favs, setFavs] = useState(true);
+    const [favsClicked, setFavsClicked] =  useState(false);
 
 
-    getFavs() {
+    const getFavs = () => {
         const getRequestOptions = {
             method: 'GET',
             headers: {'Content-Type': 'application/json'}
@@ -30,12 +24,12 @@ class CountrySelector extends Component {
             .then((res) => res.json())
             .then((data) => {
                 console.log('data received from getFavs: ', data);
-                this.setState({ favsList: data })
+                setFavsList(data);
             })
-    }
+    };
 
 
-    deletor(e) {
+    const deletor = (e) => {
         const { id } = e.target;
         const deleteRequestOptions = {
             method: 'DELETE',
@@ -47,13 +41,11 @@ class CountrySelector extends Component {
         fetch(`http://localhost:${PORT}/favorites`, deleteRequestOptions)
             .then((res) => res.json())
             .then((data) => {
-                // this.setState({ favsList: data })
-                // console.log('this.state.favsList: ', this.state.favsList);
             })
-            this.getFavs();
-    }
+            getFavs();
+    };
 
-    visitUnvisit(e) {
+    const visitUnvisit = (e) => {
         const { id } = e.target;
         const putRequestOptions = {
             method: 'PUT',
@@ -65,16 +57,9 @@ class CountrySelector extends Component {
         fetch(`http://localhost:${PORT}/favorites`, putRequestOptions)
             .then((res) => res.json())
             .then((data) => {
-                // this.setState({ favsList: data })
-                // console.log('this.state.favsList: ', this.state.favsList);
             })
-            this.getFavs();
-    }
-
-
-
-
-render() {
+            getFavs();
+    };
     
     const dropCountries = [];
     let country;
@@ -88,7 +73,6 @@ render() {
                 country = defaultCountries.countries[key][1];
             } else {
                 country = defaultCountries.countries[key];
-                console.log('country: ', country);
             };
             if(!countriesToExclude.includes(country)) {
                 idCount += 1;
@@ -98,65 +82,61 @@ render() {
                 }   
             };
 
+        const favorites = [];
 
-                const favorites = [];
+        if (favsList.length) {
 
-                if (this.state.favsList.length) {
-
-                    this.state.favsList.forEach((el, index) => {
-                        let greenMark = "";
-                        if (el.visited === true) {
-                            greenMark = <HiCheckCircle size={16} class='reactIcon' /> 
-                        }
-                        favorites.push(
-                            <div>
-                                <button 
-                                    type="button" 
-                                    id={el.country}
-                                    className="favsButton" 
-                                    onClick={(e) => {this.visitUnvisit(e)}}>
-                                    Visit/Unvisit
-                                </button>
-                                &nbsp;
-                                <button 
-                                    type="button" 
-                                    id={el.country}
-                                    className="favsButton" 
-                                    onClick={(e) => {this.deletor(e)}}>
-                                    Delete
-                                </button>
-                                &nbsp;&nbsp;&nbsp;
-                                {el.country}
-                                {greenMark}
-                            </div>
-                        )
-                    })
-                } else favorites.push(<h3>Add some Favorites to get started!</h3>)
+            favsList.forEach((el, index) => {
+                // REFACTOR THIS WITH BOOLEAN STATEMENT && to recognize either first false statement or last true one
+                let greenMark = "";
+                if (el.visited === true) {
+                    greenMark = <HiCheckCircle size={16} className='reactIcon' /> 
+                }
+                favorites.push(
+                    <div>
+                        <button 
+                            type="button" 
+                            id={el.country}
+                            className="favsButton" 
+                            onClick={(e) => {visitUnvisit(e)}}>
+                            Visit/Unvisit
+                        </button>
+                        &nbsp;
+                        <button 
+                            type="button" 
+                            id={el.country}
+                            className="favsButton" 
+                            onClick={(e) => {deletor(e)}}>
+                            Delete
+                        </button>
+                        &nbsp;&nbsp;&nbsp;
+                        {el.country}
+                        {greenMark}
+                    </div>
+                )
+            })
+        } else favorites.push(<h3>Add some Favorites to get started!</h3>)
 
 
         
-        return (
-            <div id='CountrySelector'>
-                    <h1>Select A Country</h1>
-                    <select id='dropdown' onChange={(e) => {
-                        this.props.changeCurrCountry(e.target.value)
-                    }}>
-                        {dropCountries}
-                    </select>
-                    <div id='favs-button-div'>
-                        <button type='button' id='favsButton' onClick={this.getFavs}>
-                        View Favorites
-                        </button>
-                    </div>
-                    <div id='favs-in-selector'>
-                        {favorites}
-                    </div>
+    return (
+        <div id='CountrySelector'>
+                <h1>Select A Country</h1>
+                <select id='dropdown' onChange={(e) => {
+                    props.changeCurrCountry(e.target.value)
+                }}>
+                    {dropCountries}
+                </select>
+                <div id='favs-button-div'>
+                    <button type='button' id='favsButton' onClick={getFavs}>
+                    View Favorites
+                    </button>
                 </div>
-            )
-              
-    }
-
-
+                <div id='favs-in-selector'>
+                    {favorites}
+                </div>
+            </div>
+        )
 }
 
 
