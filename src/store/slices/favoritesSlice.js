ad4cd6ab5f164f8b9ef6d5b5d60f797d6813502b
“KelvinShamy” <kelvinshamy@gmail.com>
2025-08-26 11:34:02 -0700

    use built-in mongo id, remove comments

--- src/store/slices/favoritesSlice.js
+++ src/store/slices/favoritesSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const favoritesSlice = createSlice({
     name: favorites,
@@ -7,7 +7,6 @@ const favoritesSlice = createSlice({
             { 
                 country: 'Tierra Imaginaria',
                 visited: false,
-                id: nanoid()
             }
         ]
     },
@@ -17,14 +16,14 @@ const favoritesSlice = createSlice({
             state.favorites.push({
                 country: action.payload.country,
                 visited: action.payload.visited,
-                id: nanoid()
             });
         },
         removeFavorite (state, action) {
             console.log('favorites/removeFavorite fired w/ action:', action);
             const newState = state.favorites.filter((fav) => {
-                return fav.id !== action.payload
+                return fav._id !== action.payload
             });
+            // I can access this _id property that is auto-created by Mongo
             state.favorites = newState;
         },
     }
